61a1a8bb21a6dc74dfa2c3e5e5acd99c2a74d5e2
farid0416 <faridandi818@gmail.com>
2025-11-20 02:45:37 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
-const navbarNav = document.querySelector(".navbar-nav");
-document.querySelector("#hamburger-menu").onclick = () => {
-  navbarNav.classList.toggle("active");
-};
 const hamburger = document.querySelector('#hamburger-menu');
+const navbarNav = document.querySelector('.navbar-nav');
 
+// Toggle menu
+hamburger.addEventListener('click', function(e) {
+  e.preventDefault();     // mencegah <a> membuka halaman
+  e.stopPropagation();    // supaya klik tidak ditangkap document
+  navbarNav.classList.toggle('active');
+});
+
+// Klik di luar menu
 document.addEventListener('click', function(e) {
-  if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)){
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove('active');
   }
-})
+});
+
 
